9b38cb4e2b9b6e619623699fbbfe0a00d32a808f
Manel2005 <manel.abigil@gmail.com>
2025-11-22 23:56:29 +0100

    #devel recolecció i enviament de dades de multiples queries

--- src/queries.py
+++ src/queries.py
@@ -2,49 +2,36 @@ import requests
 import json
 
 overpass_url = 'https://overpass-api.de/api/interpreter'
-overpass_query = '''
+overpass_query_noAccess = '''
 [out:json];
 area["boundary"="administrative"]["admin_level"="8"][name="Los Angeles"]->.la;
 nwr
 '''
 
-def do_query(param):
-    global overpass_query
+overpass_query_Access = '''
+[out:json];
+area["boundary"="administrative"]["admin_level"="8"][name="Los Angeles"]->.la;
+nwr["wheelchair"="yes"]
+'''
 
-    # Amb
-    if param == "Groceries":
-        data = overpass_query+'["shop"="supermarket"](area.la);out;'
-        response = requests.get(overpass_url, params={'data': data})
-        print(response.json().get('elements'))
-    elif param == "":
-        overpass_query += '["leisure"="dog_park"]'
-    elif param == "":
-        overpass_query += '["leisure"="park"]'
-    elif param == "":
-        overpass_query += '["leisure"="nature-reserve"]'
-    elif param == "":
-        overpass_query += '["leisure"="garden"]'
-    elif param == "":
-        overpass_query += '["amenity"="nightclub"]'
-    elif param == "":
-        overpass_query += '["amenity"="pub"]'
-    elif param == "":
-        overpass_query += '["amenity"="bar"]'
-    elif param == "":
-        overpass_query += '["amenity"="kindergarten"]'
-    elif param == "":
-        overpass_query += '["amenity"="school"]'
-    elif param == "":
-        overpass_query += '["amenity"="college"]'
-    elif param == "":
-        overpass_query += '["amenity"="university"]'
-    elif param == "":
-        overpass_query += '["shop"="jewelry"]'
+def do_query(params, access=0):
+    global overpass_query_noAccess
+    global overpass_query_Access
 
-    # Yes/No Parameters'
-    if param == "Access":
-        overpass_query += '["wheelchair"="yes"]'
+    if access == 1:
+        overpass_query = overpass_query_Access
+    else:
+        overpass_query = overpass_query_noAccess
+
+    l = []
+
+    for param in params:
+        data = overpass_query + param + '(area.la);out center;'
+        response = requests.post(overpass_url, data={'data':data})
+        l.append(response.json().get('elements'))
+    return l
+
+
 
 if __name__=='__main__':
-    do_query("Sustainable")
-    do_query("Groceries")
+    print(do_query(['["amenity"="restaurant"]', '["amenity"="bar"]']))
